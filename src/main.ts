861bc3ee00a05eaadcf5750d040d73861cbb16ba
anrmathe <anrmathe@ucsc.edu>
2025-10-17 18:48:50 +0000

    Added title, canvas, and stylized canvas with thin black border, rounded corners and drop shadow. (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
+  <h1> Annette's D2 Assignment </h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Hello, world! </p>
 `;
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+document.body.append(canvas);
