9492fa460185a6b1b372737ee7750349c3562882
anrmathe <anrmathe@ucsc.edu>
2025-10-17 18:52:27 +0000

    Added detection of mousedown and mouse up, allowing user to draw as well as a button to clear the screen. (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,43 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
   <h1> Annette's D2 Assignment </h1>
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-  <p>Hello, world! </p>
 `;
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 document.body.append(canvas);
+
+const ctx = canvas.getContext("2d");
+if (!ctx) throw new Error("Could not get 2D context");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (_e) => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
